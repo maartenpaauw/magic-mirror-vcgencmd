87895cee9fff6ada6244f3a37646c570957405e5
Maarten Paauw <maartenpaauw@gmail.com>
2018-09-12 15:08:55 +0200

    style: syntax

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const exec = require('child_process').exec
+const { exec } = require('child_process')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 const command = 'vcgencmd display_power'
 
