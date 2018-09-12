90b5b992026f71f50aa9112f4453efc0533987e7
Maarten Paauw <maartenpaauw@gmail.com>
2018-09-12 14:56:17 +0200

    chore: post route

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const exec = require('child_process').exec
 
 const app = express()
 
-app.get('/api/v1/magic-mirror/display', function (req, res) {
-    exec('vcgencmd display_power', (error, stdout, stderr) => {
-        res.send(stdout.split('=')[1])
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const command = 'vcgencmd display_power'
+
+app.get('/api/v1/magic-mirror/display', (req, res) => {
+    exec(command, (error, stdout, stderr) => {
+        res.send(parseResult(stdout))
     })
 })
 
-app.post('/api/v1/magic-mirror/display-off', (req, res) => {
-    exec('vcgencmd display_power 0', (error, stdout, stderr) => {
-        res.send(stdout)
+app.post('/api/v1/magic-mirror/display', (req, res) => {
+    exec(`${command} ${req.body.state}`, (error, stdout, stderr) => {
+        res.send(parseResult(stdout))
     })
 })
 
-app.post('/api/v1/magic-mirror/display-on', (req, res) => {
-    exec('vcgencmd display_power 1', (error, stdout, stderr) => {
-        res.send(stdout)
-    })
-})
+parseResult = (res) => {
+    return res.split('=')[1]
+}
 
 app.listen(3000)
